f386002f435d42bd4b1fd6f49cd5addcc9cb4ae4
Athan Clark <athan.clark@gmail.com>
2016-07-10 16:11:45 -0600

    tests

---
+++ test/Main.hs
@@ -0,0 +1,27 @@
+module Main where
+
+import LText.Expr
+
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
+
+import Data.Text as T
+
+
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "LText test suite"
+  [ testGroup "Expr"
+      [ QC.testProperty "Print/Parse Iso" printParseIso
+      ]
+  ]
+
+
+printParseIso :: Expr -> Property
+printParseIso e =
+  ioProperty $ do
+    e' <- runParse . T.pack $ ppExpr e
+    pure $ e' == e
